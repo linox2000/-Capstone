102373fe74aca7b05e8fb847144428059b7f52e7
Manuel Lino <mllino1@hotmail.com>
2022-07-13 03:47:48 +0100

    start a new shop online App

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import './categories.styles.scss';
+
+const App = () => {
+  return (
+    <div className="categories-container">
+      <div className="category-container">
+        <div className="background-image"/>
+        <div className="category-body-container">
+          <h1>Hats</h1>
+          <p>SHOP NOW</p>
+        </div>
+      </div>
+      <div className="category-container">
+        
+        <div className="category-body-container">
+          <h1>Jackets</h1>
+          <p>SHOP NOW</p>
+        </div>
+      </div>
+      <div className="category-container">
+        
+        <div className="category-body-container">
+          <h1>Sneakers</h1>
+          <p>SHOP NOW</p>
+        </div>
+      </div>
+      <div className="category-container">
+        
+        <div className="category-body-container">
+          <h1>Womem</h1>
+          <p>SHOP NOW</p>
+        </div>
+      </div>
+      <div className="category-container">
+        
+        <div className="category-body-container">
+          <h1>Men</h1>
+          <p>SHOP NOW</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
